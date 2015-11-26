07969131b1713c05714a1a4ed30edbf6fba6b7c2
Ari Lacenski <ari@mangohealth.com>
2015-11-26 14:36:32 -0800

    Refactor EventsPerDayLogger.

--- app/src/main/java/net/tensory/powercounter/logging/EventsPerDayLogger.java
+++ app/src/main/java/net/tensory/powercounter/logging/EventsPerDayLogger.java
@@ -30,6 +30,25 @@ import java.util.List;
  * Records a log of an event in a file, storing data about the current day.
  */
 public class EventsPerDayLogger implements Logger {
+    private static class LogResult {
+        private DateTime lastLoggedDate;
+        private int lastLoggedEventCount;
+
+        public LogResult(DateTime dateTime, int value) {
+            lastLoggedDate = dateTime;
+            lastLoggedEventCount = value;
+        }
+
+        public DateTime getDateTime() {
+            return lastLoggedDate;
+        }
+
+        public int getValue() {
+            return lastLoggedEventCount;
+        }
+    }
+
+
     private static final String FILENAME = "log.txt";
     private static final String DATE_FORMAT = "dd/MM/yyyy";
     private Context context;
@@ -50,8 +69,58 @@ public class EventsPerDayLogger implements Logger {
     public void logEvent(Date eventTime) {
         File file = prepareFile();
 
+        LogResult logResult = getLastLoggedResult(file);
+
+        try {
+            DateTimeFormatter ddmmyy = DateTimeFormat.forPattern(DATE_FORMAT);
+            persist(file, String.valueOf(logResult.getValue()), ddmmyy.print(logResult.getDateTime()));
+
+            for (LogSubscriber subscriber : subscribers) {
+                subscriber.update(logResult.getValue());
+            }
+        } catch (IOException ioe) {
+            Log.e(EventsPerDayLogger.class.getName(), "Could not write file " + FILENAME, ioe);
+        }
+    }
+
+    private File prepareFile() {
+        File file = new File(context.getFilesDir(), FILENAME);
+        if (!file.exists()) {
+            OutputStream out;
+            try {
+                out = new BufferedOutputStream(new FileOutputStream(file));
+                out.close();
+                return file;
+            } catch (IOException ioe) {
+                return null;
+            }
+        }
+        return file;
+    }
+
+    private DateTime getDate(String string) throws ParseException {
+        DateTimeFormatter ddmmyy = DateTimeFormat.forPattern(DATE_FORMAT);
+        return ddmmyy.parseDateTime(string);
+    }
+
+    private static void persist(File file, String count, String date) throws IOException {
+        BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+
+        writer.write(count);
+        writer.newLine();
+        writer.write(date);
+
+        writer.flush();
+        writer.close();
+    }
+
+    private LogResult getLastLoggedResult(File file) {
         DateTime lastLoggedDate = null;
         int lastLoggedEventCount = 0;
+
+        if (file == null) {
+            return new LogResult(new DateTime(), lastLoggedEventCount);
+        }
 
         // obtain the date and the count
         try {
@@ -92,46 +161,6 @@ public class EventsPerDayLogger implements Logger {
             lastLoggedEventCount = 1;
         }
 
-        try {
-            DateTimeFormatter ddmmyy = DateTimeFormat.forPattern(DATE_FORMAT);
-            persist(file, String.valueOf(lastLoggedEventCount), ddmmyy.print(today));
-
-            for (LogSubscriber subscriber : subscribers) {
-                subscriber.update(lastLoggedEventCount);
-            }
-        } catch (IOException ioe) {
-            Log.e(EventsPerDayLogger.class.getName(), "Could not write file " + FILENAME, ioe);
-        }
-    }
-
-    private File prepareFile() {
-        File file = new File(context.getFilesDir(), FILENAME);
-        if (!file.exists()) {
-            OutputStream out;
-            try {
-                out = new BufferedOutputStream(new FileOutputStream(file));
-                out.close();
-                return file;
-            } catch (IOException ioe) {
-                return null;
-            }
-        }
-        return file;
-    }
-
-    private DateTime getDate(String string) throws ParseException {
-        DateTimeFormatter ddmmyy = DateTimeFormat.forPattern(DATE_FORMAT);
-        return ddmmyy.parseDateTime(string);
-    }
-
-    private static void persist(File file, String count, String date) throws IOException {
-        BufferedWriter writer = new BufferedWriter(new FileWriter(file));
-
-        writer.write(count);
-        writer.newLine();
-        writer.write(date);
-
-        writer.flush();
-        writer.close();
+        return new LogResult(lastLoggedDate, lastLoggedEventCount);
     }
 }
