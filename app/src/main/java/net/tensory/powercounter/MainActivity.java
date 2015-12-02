7aacc373807b10ff00f60fa5fb8c76ff99c5a5b8
Ari Lacenski <ari@mangohealth.com>
2015-12-02 12:56:48 -0800

    Activity is now invisible.

--- app/src/main/java/net/tensory/powercounter/MainActivity.java
+++ app/src/main/java/net/tensory/powercounter/MainActivity.java
@@ -3,26 +3,17 @@ package net.tensory.powercounter;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.widget.Toast;
 
 import net.danlew.android.joda.JodaTimeAndroid;
 import net.tensory.powercounter.logging.EventsPerDayLoggerFactory;
 
-import butterknife.Bind;
-import butterknife.ButterKnife;
-
 public class MainActivity extends Activity {
-
-    @Bind(R.id.event_count)
-    TextView tvEventCount;
-    @Bind(R.id.times_today)
-    TextView tvTimesToday;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ButterKnife.bind(this);
         JodaTimeAndroid.init(this.getApplicationContext());
 
         // start the PowerEventService
@@ -30,35 +21,11 @@ public class MainActivity extends Activity {
         startService(intent);
 
         LogViewer.getInstance().initialize(EventsPerDayLoggerFactory.getInstance(this.getApplicationContext()));
+        Toast.makeText(this.getApplicationContext(), getString(R.string.txt_startup_message), Toast.LENGTH_SHORT).show();
     }
-
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        // Inflate the menu; this adds items to the action bar if it is present.
-//        getMenuInflater().inflate(R.menu.menu_main, menu);
-//        return true;
-//    }
-//
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        // Handle action bar item clicks here. The action bar will
-//        // automatically handle clicks on the Home/Up button, so long
-//        // as you specify a parent activity in AndroidManifest.xml.
-//        int id = item.getItemId();
-//
-//        //noinspection SimplifiableIfStatement
-//        if (id == R.id.action_settings) {
-//            return true;
-//        }
-//
-//        return super.onOptionsItemSelected(item);
-//    }
 
     @Override
     protected void onResume() {
         super.onResume();
-
-        tvEventCount.setText(String.valueOf(LogViewer.getInstance().getCount()));
-        tvTimesToday.setText(getResources().getQuantityString(R.plurals.times_today, LogViewer.getInstance().getCount()));
     }
 }
